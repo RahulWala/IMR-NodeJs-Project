e753a0837415d81033d45773fde5dab91ba37295
Rahul Wala <31235531+RahulWala@users.noreply.github.com>
2019-05-23 00:39:30 +0530

    Add files via upload

--- dbConnect.js
+++ dbConnect.js
@@ -3,17 +3,19 @@ var app = express();
 var mysql = require("mysql");
 
 /* SQL Connection */
-var sqlConnection = mysql.createConnection({
-	host : "localhost",
-	user : "root",
-	password : 'Developer@1',
-	database : 'movie_rating'
-});
-sqlConnection.connect((err)=>{
-	if(!err)
-		console.log("Database Connected SUccessfully.");
-	else
-		console.log("Error In Database Connection.",JSON.stringify(err, undefined, 2));
-});
+	var sqlConnection = mysql.createConnection({
+		host : "localhost",
+		user : "root",
+		password : '',
+		database : 'movie_rating',
+		multipleStatements: true
+	});
+
+	sqlConnection.connect((err)=>{
+		if(!err)
+			console.log("Database Connected SUccessfully.");
+		else
+			console.log("Error In Database Connection.",JSON.stringify(err, undefined, 2));
+	});
 /*****************/
-module.exports = sqlConnection;
+module.exports = sqlConnection; 
